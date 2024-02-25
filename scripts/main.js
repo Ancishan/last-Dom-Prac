8f9252aefc9c04761004764992b09d94673960f6
Ancishan <shantodev027@gmail.com>
2024-02-25 14:00:46 +0600

    DONE complete seession

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,50 @@ for(const btn of allBtn){
     //  akhn ei khane buttton er pawa element gula k html er khali jaiga div e rakha
     
     const selectedContainer = document.getElementById("selected-player-container");
+
+
+   
+    
+
+
+
+    // cart 6 er beshi kora jabe na
+    const firstCartCount=getConvertedValue("cart");
+    if(firstCartCount + 1 > 6){
+        alert("limited");
+        return;
+    }
+    event.target.setAttribute("disabled",false);
+    event.target.parentNode.style.backgroundColor = "gray";
+
+
+
+
+// update budget and cart and left
+
+// update budget
+const budget = getConvertedValue("budget");
+if(budget > 0){
+    document.getElementById("budget").innerText = budget - parseInt(price);
+}
+else{
+    alert("your budget finished")
+}
+
+// update cart
+
+const cartCount = getConvertedValue("cart");
+document.getElementById("cart").innerText = cartCount + 1; 
+
+// update left
+const leftCount = getConvertedValue("left");
+document.getElementById("left").innerText = leftCount -1;
+
+
+
+
+
+
     // ei khane 1 ta div create krtte hbe
     const div = document.createElement("div");
     // sb seshe eita shudu lekha gula k pasa pasi rakhar jnno
@@ -59,12 +103,42 @@ for(const btn of allBtn){
     });
 }
 
+
+// apply cupon
+function updateGrandTotal(status){
+    const totalCost = getConvertedValue("total-cost");
+    if(status == undefined){
+        
+        document.getElementById("grand-total").innerText = totalCost;
+
+    }
+    else{
+     const  couponCode = document.getElementById("coupon-code").value;
+
+    if(couponCode == "love420"){
+        const discounted = totalCost * .2;
+        // console.log(discounted)
+        const result =totalCost - discounted;
+        document.getElementById("grand-total")
+.innerText = result;
+
+    }
+    else{
+        alert("please enter valid coupon code")
+    }
+
+    }
+    }
+
+   
+
+
 // grand total cost
 
-function updateGrandTotal(){
-    const totalCost = getConvertedValue("total-cost");
-    document.getElementById("grand-total").innerText = totalCost;
-}
+// function updateGrandTotal(){
+//     const totalCost = getConvertedValue("total-cost");
+//     document.getElementById("grand-total").innerText = totalCost;
+// }
 
 // total cost
 
